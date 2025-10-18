72941cda7ddfedfe4e0c1d14d991ea1bc71f88ba
umesharadhya <21f1004720@ds.study.iitm.ac.in>
2025-10-18 22:03:06 +0530

    fixed the issue

--- tests/test_evaluation.py
+++ tests/test_evaluation.py
@@ -4,6 +4,9 @@ from src.models.train import train_model
 
 def test_train_model_runs_and_returns_accuracy():
     df = generate_iris_dataframe()
-    model, acc = train_model(df)
+    model, acc, X_test, y_test = train_model(df)
     assert model is not None
     assert 0.0 <= acc <= 1.0
+    # basic sanity: predictions length matches y_test
+    preds = model.predict(X_test)
+    assert len(preds) == len(y_test)
